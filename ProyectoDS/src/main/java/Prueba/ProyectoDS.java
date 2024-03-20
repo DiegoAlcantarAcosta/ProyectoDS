b285a836082e6d897e1bc9f3ee6f14cdfd1bf9f1
DiegoAlcantarAcosta <159743139+DiegoAlcantarAcosta@users.noreply.github.com>
2024-03-19 19:20:29 -0700

    Add files via upload

---
+++ ProyectoDS/src/main/java/Prueba/ProyectoDS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package Prueba;
+
+import Interfaces.MenuPrincipal;
+
+/**
+ *
+ * @author Diego
+ */
+public class ProyectoDS {
+
+    public static void main(String[] args) {
+        MenuPrincipal m = new MenuPrincipal();
+        m.show();
+    }
+}
