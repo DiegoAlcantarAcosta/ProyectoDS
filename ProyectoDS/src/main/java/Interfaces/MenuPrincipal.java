8b345a8a10e47eb5fedaf0e78474a4986eec078c
DiegoAlcantarAcosta <haloalcantar@gmail.com>
2024-03-19 19:45:57 -0700

    MenuPrincipal casi terminado

--- ProyectoDS/src/main/java/Interfaces/MenuPrincipal.java
+++ ProyectoDS/src/main/java/Interfaces/MenuPrincipal.java
@@ -42,6 +42,16 @@ public class MenuPrincipal extends javax.swing.JFrame {
         jPanel2 = new javax.swing.JPanel();
         nombreTextField = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
+        movimientosButton = new javax.swing.JButton();
+        jLabel6 = new javax.swing.JLabel();
+        añadirContactoButton = new javax.swing.JButton();
+        jLabel7 = new javax.swing.JLabel();
+        crearGrupoButton = new javax.swing.JButton();
+        jLabel8 = new javax.swing.JLabel();
+        añadirTarjetaButton = new javax.swing.JButton();
+        transferirButton = new javax.swing.JButton();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(255, 255, 255));
@@ -114,17 +124,37 @@ public class MenuPrincipal extends javax.swing.JFrame {
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(139, 139, 139)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(nombreTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 278, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(211, Short.MAX_VALUE))
+                .addGap(166, 166, 166))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(nombreTextField, javax.swing.GroupLayout.DEFAULT_SIZE, 31, Short.MAX_VALUE)
+            .addComponent(nombreTextField, javax.swing.GroupLayout.DEFAULT_SIZE, 48, Short.MAX_VALUE)
         );
 
         jLabel3.setText("Perfil");
+
+        movimientosButton.setIcon(new javax.swing.ImageIcon("C:\\Users\\Diego\\Desktop\\ProyectoDS\\ProyectoDS\\src\\main\\java\\Imagenes\\movimientosIcon.png")); // NOI18N
+
+        jLabel6.setText("Movimientos");
+
+        añadirContactoButton.setIcon(new javax.swing.ImageIcon("C:\\Users\\Diego\\Desktop\\ProyectoDS\\ProyectoDS\\src\\main\\java\\Imagenes\\añadirContactoIcon.png")); // NOI18N
+
+        jLabel7.setText("Añadir Contacto");
+
+        crearGrupoButton.setIcon(new javax.swing.ImageIcon("C:\\Users\\Diego\\Desktop\\ProyectoDS\\ProyectoDS\\src\\main\\java\\Imagenes\\crearGrupoIcon.png")); // NOI18N
+
+        jLabel8.setText("Crear Grupo");
+
+        añadirTarjetaButton.setIcon(new javax.swing.ImageIcon("C:\\Users\\Diego\\Desktop\\ProyectoDS\\ProyectoDS\\src\\main\\java\\Imagenes\\añadirTarjetaIcon.png")); // NOI18N
+
+        transferirButton.setIcon(new javax.swing.ImageIcon("C:\\Users\\Diego\\Desktop\\ProyectoDS\\ProyectoDS\\src\\main\\java\\Imagenes\\transferirIcon.png")); // NOI18N
+
+        jLabel9.setText("Añadir Tarjeta");
+
+        jLabel10.setText("Transferir");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -153,6 +183,33 @@ public class MenuPrincipal extends javax.swing.JFrame {
                         .addComponent(jLabel3)
                         .addGap(41, 41, 41))))
             .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(36, 36, 36)
+                        .addComponent(movimientosButton)
+                        .addGap(83, 83, 83)
+                        .addComponent(añadirContactoButton))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(28, 28, 28)
+                        .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(52, 52, 52)
+                        .addComponent(jLabel7)))
+                .addGap(52, 52, 52)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(crearGrupoButton)
+                        .addGap(67, 67, 67)
+                        .addComponent(añadirTarjetaButton))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel8, javax.swing.GroupLayout.PREFERRED_SIZE, 84, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(31, 31, 31)
+                        .addComponent(jLabel9, javax.swing.GroupLayout.PREFERRED_SIZE, 84, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 43, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(transferirButton)
+                    .addComponent(jLabel10))
+                .addGap(33, 33, 33))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -181,7 +238,22 @@ public class MenuPrincipal extends javax.swing.JFrame {
                                 .addComponent(jLabel3)))))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(movimientosButton)
+                    .addComponent(añadirContactoButton)
+                    .addComponent(crearGrupoButton)
+                    .addComponent(transferirButton)
+                    .addComponent(añadirTarjetaButton, javax.swing.GroupLayout.Alignment.TRAILING))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jLabel8, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jLabel9, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jLabel10, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(256, Short.MAX_VALUE))
         );
 
         pack();
@@ -208,14 +280,24 @@ public class MenuPrincipal extends javax.swing.JFrame {
     private javax.swing.JTextField GastosTexxField;
     private javax.swing.JTextField IngresosTexxField;
     private javax.swing.JTextField SaldoDisponibleTexxField;
+    private javax.swing.JButton añadirContactoButton;
+    private javax.swing.JButton añadirTarjetaButton;
+    private javax.swing.JButton crearGrupoButton;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JButton movimientosButton;
     private javax.swing.JTextField nombreTextField;
     private javax.swing.JButton perfilButton;
+    private javax.swing.JButton transferirButton;
     // End of variables declaration//GEN-END:variables
 }
