842c5f599b0d8829ffec9deed8d49cef475bbf68
JonathanAstorga <jonathan.astorga245791@potros.itson.edu.mx>
2024-05-11 22:45:59 -0700

    Para que le des pull
    Dale pull oley

---
+++ EliminarContactoSS/src/main/java/com/mycompany/eliminarcontactoss/EliminarContactoSS.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eliminarcontactoss;
+
+/**
+ *
+ * @author Dell
+ */
+public class EliminarContactoSS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Lo mas actual");
+    }
+}
