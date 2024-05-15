1f40926f0a7fb58d8404280c53744953a565c8d9
OrlandoLeyvaFontes <orlando.leyva245512@potros.itson.edu.mx>
2024-05-15 07:16:33 -0700

    Cambio chico en boton

--- BankoPresentacion/src/main/java/Interfaces/FrmInformacionGrupo.java
+++ BankoPresentacion/src/main/java/Interfaces/FrmInformacionGrupo.java
@@ -157,12 +157,14 @@ private void llenarTablaContactos(List<Object[]> rowData) {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-FrmCrearGrupo frmCrearGrupo = new FrmCrearGrupo( personaDTO);
-    frmCrearGrupo.setVisible(true);
+    // TODO add your handling code here:
+        MenuPrincipal mp = new MenuPrincipal(personaDTO);
 
-    // Ocultar el formulario actual
-    this.setVisible(false);
-        
+        mp.show();
+        this.dispose();
+
+
+        // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
